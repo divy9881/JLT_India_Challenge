d0b57920d1d603950ec1a3cf84dc746c754a86db
DivyPatel9881 <divy9881@gmail.com>
2019-12-26 17:57:09 +0530

    Conversion files added.

---
+++ src/python/csv_to_json.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import sys
+import json
+
+filePath = sys.argv[1]
+
+data = pd.read_csv(filePath)
+
+fields = list(data)
+
+rows = len(data[fields[0]])
+cols = len(fields)
+
+dict_object = {}
+dict_object["data"] = []
+
+for i in range(rows):
+    samp_dict = {}
+    for j in range(cols):
+        samp_dict[fields[j]] = data[fields[j]][i]
+    dict_object["data"].append(samp_dict)
+
+json_object = json.dumps(dict_object,sort_keys=True, indent=4)
+
+reversedString=''.join(reversed(filePath))
+first_index = reversedString.index(".")
+reversedString = "nosj" + reversedString[first_index:]
+filePath = ''.join(reversed(reversedString))
+
+f = open(filePath, "w+")
+f.write(json_object)
+f.close()
